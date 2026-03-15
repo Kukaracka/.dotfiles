150c00245ff624f774bc8b0bb1a585f9ec39c062
Kukaracka <kukaracka@inbox.ru>
2026-03-15 20:07:21 +0700

    feat(waybar): add waybar configs

---
+++ all-configs/.config/waybar/scripts/weather.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# source https://github.com/bjesus/wttrbar
+
+import json
+import requests
+from datetime import datetime
+
+WEATHER_CODES = {
+    '113': '☀️',
+    '116': '⛅️',
+    '119': '☁️',
+    '122': '☁️',
+    '143': '🌫',
+    '176': '🌦',
+    '179': '🌧',
+    '182': '🌧',
+    '185': '🌧',
+    '200': '⛈',
+    '227': '🌨',
+    '230': '❄️',
+    '248': '🌫',
+    '260': '🌫',
+    '263': '🌦',
+    '266': '🌦',
+    '281': '🌧',
+    '284': '🌧',
+    '293': '🌦',
+    '296': '🌦',
+    '299': '🌧',
+    '302': '🌧',
+    '305': '🌧',
+    '308': '🌧',
+    '311': '🌧',
+    '314': '🌧',
+    '317': '🌧',
+    '320': '🌨',
+    '323': '🌨',
+    '326': '🌨',
+    '329': '❄️',
+    '332': '❄️',
+    '335': '❄️',
+    '338': '❄️',
+    '350': '🌧',
+    '353': '🌦',
+    '356': '🌧',
+    '359': '🌧',
+    '362': '🌧',
+    '365': '🌧',
+    '368': '🌨',
+    '371': '❄️',
+    '374': '🌧',
+    '377': '🌧',
+    '386': '⛈',
+    '389': '🌩',
+    '392': '⛈',
+    '395': '❄️'
+}
+
+data = {}
+
+
+weather = requests.get("https://wttr.in/?format=j1&lang=ru").json()
+
+
+def format_time(time):
+    return time.replace("00", "").zfill(2)
+
+
+def format_temp(temp):
+    return (hour['FeelsLikeC']+"°").ljust(3)
+
+
+def format_chances(hour):
+    chances = {
+        "chanceoffog": "Fog",
+        "chanceoffrost": "Frost",
+        "chanceofovercast": "Overcast",
+        "chanceofrain": "Rain",
+        "chanceofsnow": "Snow",
+        "chanceofsunshine": "Sunshine",
+        "chanceofthunder": "Thunder",
+        "chanceofwindy": "Wind"
+    }
+
+    conditions = []
+    for event in chances.keys():
+        if int(hour[event]) > 0:
+            conditions.append(chances[event]+" "+hour[event]+"%")
+    return ", ".join(conditions)
+
+
+data['text'] = WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
+    " "+weather['current_condition'][0]['FeelsLikeC']+"°C"
+
+data['tooltip'] = f"<b>{weather['current_condition'][0]['lang_ru'][0]['value']} {weather['current_condition'][0]['temp_C']}°</b>\n"
+data['tooltip'] += f"Ощущается как: {weather['current_condition'][0]['FeelsLikeC']}°\n"
+data['tooltip'] += f"Ветер: {weather['current_condition'][0]['windspeedKmph']} км/ч\n"
+data['tooltip'] += f"Влажность: {weather['current_condition'][0]['humidity']}%\n"
+for i, day in enumerate(weather['weather']):
+    if i == 2:
+        break
+    data['tooltip'] += f"\n<b>"
+    if i == 0:
+        data['tooltip'] += "Сегодня, "
+    if i == 1:
+        data['tooltip'] += "Завтра, "
+    data['tooltip'] += f"{day['date']}</b>\n"
+    data['tooltip'] += f"⬆️ {day['maxtempC']}° ⬇️ {day['mintempC']}° "
+    data['tooltip'] += f"🌅 {day['astronomy'][0]['sunrise']} 🌇 {day['astronomy'][0]['sunset']}\n"
+    for hour in day['hourly']:
+        if i == 0:
+            if int(format_time(hour['time'])) < datetime.now().hour-2:
+                continue
+        data['tooltip'] += f"{format_time(hour['time'])} {WEATHER_CODES[hour['weatherCode']]} {format_temp(hour['FeelsLikeC'])} {hour['lang_ru'][0]['value']}\n"
+
+
+
+
+print(json.dumps(data))
